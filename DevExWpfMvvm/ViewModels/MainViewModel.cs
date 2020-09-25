dc2dcadcbbbfa0d42fc4f81f0532b2e9b50d153b
Mozart Al Khateeb <m.alkhateeb@ids.com.lb>
2020-09-25 14:18:25 +0300

    Simple MVVM Example Using Bindable Base

---
+++ DevExWpfMvvm/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.Mvvm;
+
+namespace DevExWpfMvvm.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+
+        public MainViewModel()
+        {
+
+        }
+
+        public string FirstName
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value, changedCallback: NotifyFullNameChanged); }
+        }
+
+        public string LastName
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value, changedCallback: NotifyFullNameChanged); }
+        }
+
+        public string FullName { get { return FirstName + " " + LastName; } }
+
+        void NotifyFullNameChanged()
+        {
+            RaisePropertyChanged(() => FullName);
+        }
+
+        void Save()
+        {
+            //Save Changes to database
+        }
+    }
+}
